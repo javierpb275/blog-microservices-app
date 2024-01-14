122b0302a5f0ca78bd4177ede8abce772ca8dc95
javierpb275 <javierpb275@gmail.com>
2024-01-14 20:20:04 +0100

    created moderation service to show if a comment is approved rejected or pending moderation

--- comments/index.js
+++ comments/index.js
@@ -17,7 +17,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = COMMENTS_BY_POST_ID[req.params.id] || [];
-  comments.push({ id, content });
+  comments.push({ id, content, status: "pending" });
   COMMENTS_BY_POST_ID[req.params.id] = comments;
   await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
@@ -25,13 +25,36 @@ app.post("/posts/:id/comments", async (req, res) => {
       id,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
   res.status(201).send({ id, content });
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event Received", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { id, postId, status, content } = data;
+    const comments = COMMENTS_BY_POST_ID[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
+
   res.send({});
 });
 
