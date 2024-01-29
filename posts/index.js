6bdfb678a574f29d22256d371ba1ceb6a2f38b5f
javierpb275 <javierpb275@gmail.com>
2024-01-29 21:19:56 +0100

    updating service addresses

--- posts/index.js
+++ posts/index.js
@@ -17,7 +17,7 @@ app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   POSTS[id] = { id, title };
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
