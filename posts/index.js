7fee70721259ca3a3557b473917caadd82cb799d
javierpb275 <javierpb275@gmail.com>
2024-02-19 20:38:02 +0100

    route configuration

--- posts/index.js
+++ posts/index.js
@@ -13,7 +13,7 @@ app.get("/posts", (req, res) => {
   res.status(200).send(POSTS);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   POSTS[id] = { id, title };
